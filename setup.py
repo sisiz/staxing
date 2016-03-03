36ad44be791d48a2341d324e874c5e2cb71f4509
Greg Fitch <greg.fitch@rice.edu>
2016-03-03 13:20:03 -0600

    update version number for the previous commit

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='Staxing',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     scripts=[],
     # zip_safe=True,
