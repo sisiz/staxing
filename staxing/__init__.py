55f3016d71af7e297d142c2b0eeda0af52da2bb5
Greg Fitch <greg.fitch@rice.edu>
2016-03-31 16:12:45 -0500

    Update __init__.py

--- staxing/__init__.py
+++ staxing/__init__.py
@@ -1,11 +1,9 @@
-from .staxing import Stax
 from .helpers import StaxHelper, Admin, Student, Teacher
 from .assignment import Assignment
 
 if __name__ == '__main__':
-    a = Stax
-    b = StaxHelper
-    c = Assignment
-    d = Admin
-    e = Student
-    f = Teacher
+    a = StaxHelper
+    b = Assignment
+    c = Admin
+    d = Student
+    e = Teacher
