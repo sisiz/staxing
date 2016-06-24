751146a36ddb8b5ce7f17b84b38652baa3751f8e
Greg Fitch <greg.fitch@rice.edu>
2016-06-24 13:42:12 -0500

    assignment.py driver corrections

--- staxing/assignment.py
+++ staxing/assignment.py
@@ -230,14 +230,15 @@ class Assignment(object):
         if 'all' in periods:
             opens_on, closes_on = periods['all']
             today = datetime.date.today()
-            self.teacher.driver.find_element(
+            driver.find_element(
                 By.XPATH, '//div[contains(@class,"-due-date")]' +
-                '//div[contains(@class,"datepicker__input")]').click()
+                '//div[contains(@class,"datepicker__input")]'
+            ).click()
             # get calendar to correct month
             month = today.month
             year = today.year
             while (month != int(closes_on[:2]) or year != int(closes_on[6:])):
-                self.teacher.driver.find_element(
+                driver.find_element(
                     By.XPATH,
                     '//a[contains(@class,"navigation--next")]'
                 ).click()
@@ -246,22 +247,24 @@ class Assignment(object):
                 else:
                     month = 1
                     year += 1
-            self.teacher.driver.find_element(
+            driver.find_element(
                 By.XPATH, '//div[contains(@class,"datepicker__day")' +
-                'and contains(text(),"' + (closes_on[3:5]) + '")]').click()
+                'and contains(text(),"' + (closes_on[3:5]) + '")]'
+            ).click()
             time.sleep(0.5)
-            self.teacher.driver.find_element(
+            driver.find_element(
                 By.CLASS_NAME,
                 'assign-to-label'
             ).click()
-            self.teacher.driver.find_element(
+            driver.find_element(
                 By.XPATH, '//div[contains(@class,"-open-date")]' +
-                '//div[contains(@class,"datepicker__input")]').click()
+                '//div[contains(@class,"datepicker__input")]'
+            ).click()
             # get calendar to correct month
             month = today.month
             year = today.year
             while (month != int(opens_on[:2]) or year != int(opens_on[6:])):
-                self.teacher.driver.find_element(
+                driver.find_element(
                     By.XPATH,
                     '//a[contains(@class,"navigation--next")]'
                 ).click()
@@ -270,13 +273,13 @@ class Assignment(object):
                 else:
                     month = 1
                     year += 1
-            self.teacher.driver.find_element(
+            driver.find_element(
                 By.XPATH,
                 '//div[contains(@class,"datepicker__day")' +
                 'and contains(text(),"%s")]' % opens_on[3:5]
             ).click()
             time.sleep(0.5)
-            self.teacher.driver.find_element(
+            driver.find_element(
                 By.CLASS_NAME,
                 'assign-to-label'
             ).click()
@@ -291,21 +294,20 @@ class Assignment(object):
                 if periods[period] is 'all' or period is 'skip':
                     continue
                 opens_on, closes_on = periods[period]
-
                 driver.find_element(
                     By.XPATH,
                     '//input[@id="period-toggle-%s"]' % count +
                     '/../following-sibling::div/following-sibling::div' +
-                    '//input[contains(@class,"picker")]'). \
-                    send_keys(closes_on)
+                    '//input[contains(@class,"picker")]'
+                ).send_keys(closes_on)
                 time.sleep(0.5)
                 driver.find_element(By.CLASS_NAME, 'assign-to-label').click()
                 driver.find_element(
                     By.XPATH,
                     '//input[@id="period-toggle-%s"]' % count +
                     '/../following-sibling::div' +
-                    '//input[contains(@class,"picker")]'). \
-                    send_keys(opens_on)
+                    '//input[contains(@class,"picker")]'
+                ).send_keys(opens_on)
                 time.sleep(0.5)
                 driver.find_element(By.CLASS_NAME, 'assign-to-label').click()
 
