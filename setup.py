80391310c6c94bdf25dbc25c4751ab13e9330b52
Greg Fitch <greg.fitch@rice.edu>
2016-06-24 13:44:39 -0500

    forgot version bump again

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='Staxing',
-    version='0.0.18',
+    version='0.0.19',
     packages=find_packages(),
     scripts=[],
     # zip_safe=True,
