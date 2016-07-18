a69b00e2a672ecb5600b0f8ddb5c4e1f27360b3e
Greg Fitch <greg.fitch@rice.edu>
2016-07-18 10:01:15 -0500

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='Staxing',
-    version='0.0.21',
+    version='0.0.22',
     packages=find_packages(),
     scripts=[],
     # zip_safe=True,
